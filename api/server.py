fc19ba01fe26d1697f79c5e3eaae0e30e0932974
ネコにいさんチャンネル <nekoniisanb@gmail.com>
2024-11-22 11:04:04 +0900

    Update server.py

--- api/server.py
+++ api/server.py
@@ -19,7 +19,7 @@ import vercel_blob
 # 定数
 MAX_COUNT = 20
 INIT_DATA_PATH = "data/init_data.json"
-TMP_FOLDER = "./tmp"
+TMP_FOLDER = "/tmp"
 
 app = Flask(__name__)
 app.secret_key = 'abcdefghijklmn'
