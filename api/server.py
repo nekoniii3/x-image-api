5fe365cc8b05f1bb6f04e599e474f871c54a28bc
ネコにいさんチャンネル <nekoniisanb@gmail.com>
2024-11-29 09:42:20 +0900

    Update server.py

--- api/server.py
+++ api/server.py
@@ -29,8 +29,8 @@ CORS(app, supports_credentials=True)
 
 @app.after_request
 def after_request(response):
-    # response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
-    response.headers.add('Access-Control-Allow-Origin', 'https://x-image-gallery-git-dev-nekoniii3s-projects.vercel.app')
+    response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+    # response.headers.add('Access-Control-Allow-Origin', 'https://x-image-gallery-git-dev-nekoniii3s-projects.vercel.app')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,X-Custom-Header')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     response.headers.add('Access-Control-Allow-Credentials', 'true')
